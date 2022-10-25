13cf7ed9e27450005638c4ecea0f937a34a58072
Daun <daunje0@gmail.com>
2022-10-26 01:52:30 +0900

    1

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -1,2 +1,25 @@
-package hello.typeconverter;public class WebConfig {
+package hello.typeconverter;
+
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    /**MyNumberFormatter는 문자 <-> 숫자로 바꿔주는 역할인데 위 두 컨버터도 같은 역할
+     * 우선순위와 중복 이슈로 주석 처리함.*/
+/*    registry.addConverter(new StringToIntegerConverter());
+    registry.addConverter(new StringToIntegerConverter());*/
+    registry.addConverter(new StringToIpPortConverter());
+    registry.addConverter(new IpPortToStringConverter());
+
+    registry.addFormatter(new MyNumberFormatter());
+  }
 }
--- src/main/java/hello/typeconverter/converter/IpPortToStringConverter.java
+++ src/main/java/hello/typeconverter/converter/IpPortToStringConverter.java
@@ -1,2 +1,14 @@
-package hello.typeconverter;public class IpPortToStringConverter {
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.converter.GenericConverter;
+
+@Slf4j
+public class IpPortToStringConverter implements Converter<IpPort, String> {
+  @Override
+  public String convert(IpPort source) {
+    log.info("convert source={}", source);
+    return source.getIp() + ":" + source.getPort(); }
 }
